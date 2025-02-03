dd2bc2d2a320378e2219b8639c5810bbe766a010
brianna-mitri <brianna.mitri@gmail.com>
2025-02-02 21:02:17 -0800

    Added interactive buttons for location

--- Work/Web/static/js/app.js
+++ Work/Web/static/js/app.js
@@ -1,10 +1,11 @@
 // define url
 const baseURL = 'https://bmitri.pythonanywhere.com/api/v1.0/';
 const optionsURL = baseURL + 'options';
-//const optionsURL = `${baseURL}options`;
 const countiesURL = baseURL + 'counties';
-//const apiURL = 'https://bmitri.pythonanywhere.com/api/v1.0/dashboard?start_year=2020&duration=1&state=AL';
 const dashboardURL = baseURL + 'dashboard';
+
+// store entire counties
+let allCounties = [];
 
 /*****************************************
  * DOM References
@@ -12,10 +13,14 @@ const dashboardURL = baseURL + 'dashboard';
 const dashboardForm = document.getElementById('dashboardForm');
 const startYearDropdown = document.getElementById('startYearDropdown');
 const durationDropdown = document.getElementById('durationDropdown');
+const stateDropdown = document.getElementById('stateDropdown');
+const countyDropdown = document.getElementById('countyDropdown');
+
 
 /*****************************************
  * Populate drowpdown options function
  *****************************************/
+// for non county dropdowns
 function populateDropdown(selectElement, items, placeholder) {
     // clear existing options
     selectElement.innerHTML = '';
@@ -35,23 +40,75 @@ function populateDropdown(selectElement, items, placeholder) {
     });
 }
 
+// for county
+function populateCountyDropdown(selectElement, items, placeholder) {
+    // clear existing options
+    selectElement.innerHTML = '';
+
+    // add placeholder option
+    const placeholderOption = document.createElement('option');
+    placeholderOption.value = '';
+    placeholderOption.textContent = placeholder;
+    selectElement.appendChild(placeholderOption);
+
+    // add each county
+    items.forEach((item) => {
+        const option = document.createElement('option');
+        option.value = item.FIPS;  //fips code (hidden value)
+        option.textContent = item.COUNTYNAME;  //display name
+        selectElement.appendChild(option);
+    });
+}
+
 /*****************************************
  * Fetch options from API (on page load)
  *****************************************/
 window.addEventListener('DOMContentLoaded', () => {
     // fetch options route for years and states
     fetch(optionsURL)
-    .then((res) => res.json())
-    .then((data) => {
-        // populate start year and state dropdowns
-        populateDropdown(startYearDropdown, data.years, 'Select...');
-        //populateDropdown(startYearDropdown, data.states, 'Select State');
-    })
-    .catch((err) => console.error('Error fetching options', err));
+        .then((res) => res.json())
+        .then((data) => {
+            // populate start year and state dropdowns
+            populateDropdown(startYearDropdown, data.years, 'Select...');
+            populateDropdown(stateDropdown, data.states, 'Select...');
+        })
+        .catch((err) => console.error('Error fetching options', err));
+
+    // fetch counties route from api
+    fetch(countiesURL)
+        .then((res) => res.json())
+        .then((counties) => {
+            // populate counties array --> [{FIPS, COUNTYNAME, STATE},..]
+            allCounties = counties;
+        })
+        .catch((err) => console.error('Error fetching counties', err));
+
 
     // hardcode duration dropdown options (1-10 years)
-    const durations = [1,2,3,4,5,6,7,8,9,10];
+    const durations = Array.from({length:10}, (_, i) => i + 1);
     populateDropdown(durationDropdown, durations, 'Select...');
+});
+
+/*****************************************
+ * Filter counties dropdown based on state
+ *****************************************/
+stateDropdown.addEventListener('change', () => {
+    // capture selected state
+    const selectedState = stateDropdown.value;
+    if (!selectedState) {
+        // reset county dropdown if user cleared selection
+        countyDropdown.innerHTML = "<option value=''>Select a State first...</option>";
+        return;
+    }
+
+    // filter counties based on state
+    const filtered = allCounties.filter(
+        (c) => c.STATE == selectedState
+    );
+
+    // convert to a list of just county names & populate dropdown
+    populateCountyDropdown(countyDropdown, filtered, 'Select...');
+
 });
 
 /*****************************************
@@ -65,6 +122,8 @@ dashboardForm.addEventListener('submit', function (event) {
     // collect values
     const startYear = startYearDropdown.value;
     const duration = durationDropdown.value;
+    const state = stateDropdown.value;
+    const fips = countyDropdown.value;
 
     // validate required fields
     if (!startYear || !duration) {
@@ -73,11 +132,21 @@ dashboardForm.addEventListener('submit', function (event) {
     }
 
     // build final dashboard url api call
+    //required endpoints
     let params = new URLSearchParams({
         start_year: startYear,
         duration: duration
     });
 
+    // optional endpoints
+    if (state) {
+        params.append('state', state);
+    }
+    if (fips) {
+        params.append('fips', fips);
+    }
+
+    // final API URL
     const finalURL = `${dashboardURL}?${params.toString()}`
 
     // fetch data
